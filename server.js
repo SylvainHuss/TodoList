8d72b97bde6face6bb219c8ae6d9199388e00fe7
SylvainHuss <sylvain.huss@laposte.net>
2019-04-11 12:11:53 +0200

    gitignoremaj

--- server.js
+++ server.js
@@ -6,17 +6,21 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var ObjectId = require('mongodb').ObjectID;
-var session = require('express-session');
+//var session = require('express-session');
 var sha1 = require('js-sha1');
 
 /* Initialisation : */
 var app = express();
+var cors = require('cors');
 
+app.use(cors());
 app.use(express.static(__dirname + '/public')); //Dossier des données statics
 app.use(morgan('dev')); //color output for development usage
 app.use(bodyParser.urlencoded({'extended':'true'})); 
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type : 'application/vnd.api+json' })); //type de l'application
+
+let session = "";
 
 function ignoreFavicon(req, res, next) {
     if (req.originalUrl === '/favicon.ico') {
@@ -34,15 +38,15 @@ dataLayer.init(function(){
     console.log("on utilise le port 8888");
 });
 
-app.use(session({
-    resave: false, // don't save session if unmodified
-    saveUninitialized: false, // don't create session until something stored
-    secret: 'keyboard cat'
-}));
+// app.use(session({
+//     resave: false, // don't save session if unmodified
+//     saveUninitialized: false, // don't create session until something stored
+//     secret: 'keyboard cat'
+// }));
 
 app.get('/api/laliste', function(req, res){
-    if (typeof req.session.username != "undefined"){
-        dataLayer.getTaskSet(req.session.username, function(data){
+    if (session != ""){
+        dataLayer.getTaskSet(session, function(data){
             res.send(data);
         });
     }
@@ -52,7 +56,7 @@ app.get('/api/laliste', function(req, res){
 });
 
 app.get('/api/user', function(req, res){
-    res.send(req.session.username);
+    res.send(session);
 });
 
 app.get('/taches', function(req, res){
@@ -60,12 +64,12 @@ app.get('/taches', function(req, res){
 });
 
 app.get('/accueil', function(req, res){
-    req.session.destroy();
+    session = "";
     res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/api/category', function(req, res){
-    dataLayer.getCategorySet(req.session.username, function(data){
+    dataLayer.getCategorySet(session, function(data){
         res.send(data);
     });
 });
@@ -77,11 +81,11 @@ app.post('/api/laliste/:category/:newtask', function(req, res){
             text : req.params.newtask,
             done : false,
             date : new Date().toLocaleTimeString('en-GB', {formatMatcher: 'basic'}),
-            creator : req.session.username,
+            creator : session,
             category: req.params.category
         };
         dataLayer.insertTask(task, function(){
-            dataLayer.getTaskSet(req.session.username, function(data){
+            dataLayer.getTaskSet(session, function(data){
                 res.send(data);
             });
         });
@@ -93,10 +97,10 @@ app.post('/api/category/:category', function(req, res){
 
         var newcategory = {
             text : req.params.category,
-            creator : req.session.username
+            creator : session
         };
         dataLayer.insertCategory(newcategory, function(){
-            dataLayer.getCategorySet(req.session.username, function(data){
+            dataLayer.getCategorySet(session, function(data){
                 res.send(data);
             });
         });
@@ -113,7 +117,7 @@ app.post('/api/user/', function(req, res){
                     username : req.body.username,
                     password : sha1(req.body.password)
                 };
-                req.session.username = req.body.username;
+                session = req.body.username;
                 res.send(user);
             }
         });
@@ -141,7 +145,7 @@ app.post('/api/createuser/', function(req, res){
 app.delete('/api/laliste/:liste_id', function(req, res){
     var id = new ObjectId(req.params.liste_id);
     dataLayer.removeTask(id, function(){
-        dataLayer.getTaskSet(req.session.username, function(data){
+        dataLayer.getTaskSet(session, function(data){
             res.send(data);
         });
     });
@@ -150,7 +154,7 @@ app.delete('/api/laliste/:liste_id', function(req, res){
 app.delete('/api/category/:category_id', function(req, res){
     var id = new ObjectId(req.params.category_id);
     dataLayer.removeCategory(id, function(){
-        dataLayer.getCategorySet(req.session.username, function(data){
+        dataLayer.getCategorySet(session, function(data){
             res.send(data);
         });
     });
@@ -160,7 +164,7 @@ app.put('/api/laliste/:liste_id', function(req, res){
     var id = new ObjectId(req.params.liste_id);
     
     dataLayer.checkTask(id, function(){
-        dataLayer.getTaskSet(req.session.username, function(data){
+        dataLayer.getTaskSet(session, function(data){
             res.send(data);
         });
     });
@@ -169,7 +173,7 @@ app.put('/api/laliste/:liste_id', function(req, res){
 app.put('/api/laliste/todo/:liste_id/:newtext', function(req, res){
     var id = new ObjectId(req.params.liste_id);
     dataLayer.changeName(id, req.params.newtext, function(){
-        dataLayer.getTaskSet(req.session.username, function(data){
+        dataLayer.getTaskSet(session, function(data){
             res.send(data);
         });
     });
